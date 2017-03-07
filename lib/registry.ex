f7564eab3dbea7082853d78550b12286abaed173
Manjunath Reddy <manjunath.chk@gmail.com>
2017-03-08 00:12:28 +0800

    Update registry with monitor

--- lib/registry.ex
+++ lib/registry.ex
@@ -39,23 +39,23 @@ defmodule KV.Registry do
     def init(:ok) do
         names = %{}
         refs  = %{}
-        {:ok, %{names, refs}}
+        {:ok, {names, refs}}
     end
 
     def handle_call({:lookup, name}, _from, {names, _} = state) do
-    {:reply, Map.fetch(names, name), state}
+        {:reply, Map.fetch(names, name), state}
     end
 
     def handle_cast({:create, name}, {names, refs}) do
-    if Map.has_key?(names, name) do
-        {:noreply, {names, refs}}
-    else
-        {:ok, pid} = KV.Bucket.start_link
-        ref = Process.monitor(pid)
-        refs = Map.put(refs, ref, name)
-        names = Map.put(names, name, pid)
-        {:noreply, {names, refs}}
-    end
+        if Map.has_key?(names, name) do
+            {:noreply, {names, refs}}
+        else
+            {:ok, pid} = KV.Bucket.start_link
+            ref = Process.monitor(pid)
+            refs = Map.put(refs, ref, name)
+            names = Map.put(names, name, pid)
+            {:noreply, {names, refs}}
+        end
     end
 
     def handle_cast({:create, name}, names) do
